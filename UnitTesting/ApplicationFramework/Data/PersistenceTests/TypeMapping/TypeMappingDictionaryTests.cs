75223de07405ba7b6fe77bda60052115451cc340
james-dibble <james@jdibble.co.uk>
2013-06-03 20:58:58 +0100

    Type mapping configuration and testing.

---
+++ UnitTesting/ApplicationFramework/Data/PersistenceTests/TypeMapping/TypeMappingDictionaryTests.cs
@@ -0,0 +1,57 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="TypeMappingDictionaryTests.cs" company="James Dibble">
+//    Copyright 2012 James Dibble
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+namespace JamesDibble.UnitTesting.ApplicationFramework.Data.PersistenceTests.TypeMapping
+{
+    using JamesDibble.ApplicationFramework.Data.Persistence;
+    using JamesDibble.ApplicationFramework.Data.Persistence.TypeMapping;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    /// <summary>
+    /// The type mapping dictionary tests.
+    /// </summary>
+    [TestClass]
+    public class TypeMappingDictionaryTests
+    {
+        private TypeMappingDictionary _target;
+
+        /// <summary>
+        /// The test start-up.
+        /// </summary>
+        [TestInitialize]
+        public void TestStartup()
+        {
+            this._target = new TypeMappingDictionary();
+        }
+
+        /// <summary>
+        /// The test add.
+        /// </summary>
+        [TestMethod]
+        public void TestAdd()
+        {
+            var expected = typeof(FakeObject);
+
+            this._target.Add<IFakeObject, FakeObject>();
+
+            Assert.AreEqual(this._target[typeof(IFakeObject)], expected);
+        }
+
+        /// <summary>
+        /// The FakeObject interface.
+        /// </summary>
+        public interface IFakeObject : IPersistedObject
+        {
+        }
+
+        /// <summary>
+        /// The fake object.
+        /// </summary>
+        public class FakeObject : IFakeObject
+        {
+        }
+    }
+}
