ccce8599498ef29c0e29af70b286756b41a91d88
james-dibble <james@jdibble.co.uk>
2013-08-05 21:54:53 +0100

    Adding some extensions for various things.

---
+++ ApplicationFramework/Web/Javascript/scrollToTop.js
@@ -0,0 +1,51 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="scrollToTop.js" company="James Dibble">
+//   Copyright 2012 James Dibble
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+(function ($) {
+    'use strict';
+
+    $.fn.scrollToTop = function (options) {
+
+        var defaults = {
+            animate: true,
+            animationDuration: 500,
+            offest: 0,
+            easing: '',
+            callback: function () {
+            }
+        };
+
+        var scrollToTopLinks = $(this);
+
+        options = $.extend(defaults, options);
+
+        $(scrollToTopLinks).click(function () {
+
+            var executeCallback = false;
+
+            var callbackWrapper = function () {
+                if (executeCallback) {
+                    options.callback();
+                }
+
+                executeCallback = !executeCallback;
+            };
+
+            var animationOptions = {
+                duration: options.animate ? options.animationDuration : 0,
+                easing: options.easing,
+                complete: callbackWrapper
+            };
+
+            $('body,html').animate({
+                scrollTop: options.offest
+            },
+                animationOptions
+            );
+
+            return false;
+        });
+    };
+})(jQuery);
